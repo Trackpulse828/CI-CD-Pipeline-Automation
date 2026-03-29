25300b931b57eb82d9c5bc90d0a0beb5c8f5f28d
ITSprakrati <raviprakratiarya28@gmail.com>
2026-03-30 00:33:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("CI/CD working🚀");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+  res.send("CI/CD working 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
